b32bf0cd396c9683ac77edf936bd6620603df31a
Autumn Skye <abilby@mail.valenciacollege.edu>
2025-09-05 21:43:43 -0400

    Update FineLimitExceededException.java Adding comments to meet assignment requirements

--- src/FineLimitExceededException.java
+++ src/FineLimitExceededException.java
@@ -1,4 +1,16 @@
+/*
+ * Autumn Skye
+ * CEN-3024C 13950
+ * September 3rd, 2025
+ *
+ * Exception class used when a fine exceeds $250
+ */
+
 public class FineLimitExceededException extends Exception {
+    /* Method Name: Fine Limit Exceeded Exception
+     * Purpose: Used to return to the CMI when a fine exceeds $250
+     * Parameters: A string message from the Patrons Class
+     */
     public FineLimitExceededException(String message) {
         super(message);
     }
